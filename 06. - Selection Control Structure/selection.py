a76e1e47358d1136cdbef71092e50d36e98b6a07
Kalutu <kalutudaniel@gmail.com>
2024-06-29 07:36:38 +0300

    Selection

---
+++ 06. - Selection Control Structure/selection.py
@@ -0,0 +1,25 @@
+# age = 19
+
+# if age>=18:
+#     print("You are an adult")
+
+
+# if age>=18:
+#     print("You are an adult")
+# else:
+#     print("You are a child")
+
+score = 67
+
+if score >=90:
+    print("Grade A")
+elif score>=80:
+    print("Grade B")
+elif score>=70:
+    print("Grade C")
+elif score>=60:
+    print("Grade D")
+elif score>=50:
+    print("Grade E")
+else:
+    print("Grade F")
