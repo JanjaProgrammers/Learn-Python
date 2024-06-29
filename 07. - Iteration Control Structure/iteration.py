28b20591b39cff977660ab08fbe9cde7e7c9cb34
Kalutu <kalutudaniel@gmail.com>
2024-06-29 18:16:59 +0300

    loops

---
+++ 07. - Iteration Control Structure/iteration.py
@@ -0,0 +1,18 @@
+# print("0")
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+# print("5")
+
+# count = 100
+# while count>=0:
+#     print("Sorry")
+#     count = count - 1
+
+#ICU
+
+name = "Mary"
+
+for char in name:
+    print(char)
