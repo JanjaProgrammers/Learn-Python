ead104c26ae7b9d13529ff1f03aba029c2379f05
Kalutu <kalutudaniel@gmail.com>
2024-07-04 14:09:38 +0300

    Dictionaries

---
+++ 18 - Dictionaries/dict.py
@@ -0,0 +1,14 @@
+emails = {
+    "dan":"dan@jp.com",
+    "mary":"mary@email.com",
+    "staff_emails" : {
+        "john":"john@jp.com",
+        "jane":"jane@email.com"
+    }
+}
+
+print(emails["staff_emails"]["john"])
+
+
+
+
