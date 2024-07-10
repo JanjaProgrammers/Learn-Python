3be2fb466ab6451cab1a8095376528ffa779ec1b
Kalutu <kalutudaniel@gmail.com>
2024-07-10 13:42:47 +0300

    Try except block

--- 25 - Try Except block/main.py
+++ 25 - Try Except block/main.py
@@ -1,6 +1,21 @@
-number1 = float(input("Enter first number: "))
-number2 = float(input("Enter second number: "))
+try:
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
 
-qoutient = number1 / number2
+    qoutient = number1 / number2
 
-print(f"The quotient of {number1} and {number2} is {qoutient}")
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+except ValueError as e:
+    print(e)
+    print("Only numbers are required as input")
+
+except Exception:
+    print("Something went wrong")
+
+else:
+    print(f"The quotient of {number1} and {number2} is {qoutient}")
+
+finally:
+    print("This line will execude regradless")
